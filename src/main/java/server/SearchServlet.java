1a7ecd4713e4179f4c37de3b5c016cd8bc1e6898
marcus0329 <skuan@dons.usfca.edu>
2023-11-28 17:08:17 -0800

    Finished

--- src/main/java/server/SearchServlet.java
+++ src/main/java/server/SearchServlet.java
@@ -1,6 +1,8 @@
 package server;
 
 import database.DatabaseHandler;
+import hotelapp.Hotel;
+import hotelapp.ThreadSafeHotelSearcher;
 import org.apache.commons.text.StringEscapeUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -13,69 +15,50 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        /*response.setContentType("text/html");
+        response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         HttpSession session = request.getSession();
         PrintWriter out = response.getWriter();
 
         String username = (String) session.getAttribute("username");
-        if (username != null) {
-            out.println(username);
+        if (username == null) {
+            response.sendRedirect("/portal");
+            return;
         }
+
+        String hotelName = request.getParameter("hotelName");
+        hotelName = StringEscapeUtils.escapeHtml4(hotelName);
+
+        ThreadSafeHotelSearcher searcher = (ThreadSafeHotelSearcher) getServletContext().getAttribute("hotelSearcher");
 
         VelocityEngine ve = (VelocityEngine) getServletContext().getAttribute("templateEngine");
         VelocityContext context = new VelocityContext();
-        //Template template = ve.getTemplate("templates/hotelInfo.html");
-        Template template = ve.getTemplate("templates/CATravel.html");
-//		context.put("name", name);
+        StringWriter writer = new StringWriter();
+        Template template = ve.getTemplate("templates/Searching.html");
 
-        String title = "Registration Servlet";
-        String header = "<!DOCTYPE html\n>" + "	<head>\n" + "<title>" + title + "</title>\n" + "</head>\n";
+        List<Hotel> hotels;
+        if (hotelName == null) {
+            hotels = (List<Hotel>) searcher.getHotels();
+        } else {
+            hotels = searcher.search(hotelName);
+        }
+        context.put("hotels", hotels);
 
-        String body = "	<body>\n" + "<p>Hello! You have visited " + " time(s).</p>\n";
+        template.merge(context, writer);
+        out.println(writer);
 
-        body = body + "	</body>\n";
-        String footer = "</html>";
-
-        String page = header + body + footer;
-        out.println(page);
-
-        String format = "yyyy-MM-dd hh:mm a";
-        DateFormat formatter = new SimpleDateFormat(format);
-        username = formatter.format(Calendar.getInstance().getTime());
-        session.setAttribute("date", username);
-
-
-        response.setStatus(HttpServletResponse.SC_OK);*/
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        HttpSession session = request.getSession();
 
-        String username = request.getParameter("username");
-        username = StringEscapeUtils.escapeHtml4(username);
-        String password = request.getParameter("pass");
-        password = StringEscapeUtils.escapeHtml4(password);
-
-        DatabaseHandler dbHandler = DatabaseHandler.getInstance();
-        dbHandler.registerUser(username, password);
-
-        response.getWriter().println("Successfully registered the user " + username);
-        session.setAttribute("username", username);
-        response.sendRedirect("/search");
-        // Can redirect to another page here.
     }
 }
