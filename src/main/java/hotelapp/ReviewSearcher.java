3c016cd40c95972fbf560776b8f57f719519f3ff
marcus0329 <skuan@dons.usfca.edu>
2023-12-06 10:53:46 -0800

    Combined with ThreadSafeReviewSearcher

--- src/main/java/hotelapp/ReviewSearcher.java
+++ src/main/java/hotelapp/ReviewSearcher.java
@@ -1,34 +1,44 @@
 package hotelapp;
 
 import com.google.gson.Gson;
-import org.javatuples.Triplet;
+import database.DatabaseHandler;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * The main searching class containing review search methods and several data structure for storing data
  */
-public abstract class ReviewSearcher {
+public class ReviewSearcher {
 
-    protected HashMap<String, TreeSet<Review>> reviews;
+    private ExecutorService poolManager = Executors.newFixedThreadPool(4);
+    private Phaser phaser = new Phaser();
+
 
     /**
      * Constructor without calling parseDirectory
      */
-    public ReviewSearcher() {
-        this.reviews = new HashMap<>();
-    }
+    public ReviewSearcher() {}
 
-    /**
-     * Constructor with calling parseDirectory
-     * @param path the review directory to be parsed
-     */
-    public ReviewSearcher(String path) {
-        this.reviews = new HashMap<>();
+    public ReviewSearcher (String path) {
+        this.parseDirectoryMultiThread(path);
+        phaser.awaitAdvance(0);
+        poolManager.shutdown();
+        try {
+            poolManager.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -41,12 +51,12 @@ public abstract class ReviewSearcher {
 
         try (FileReader fr = new FileReader(filePath)) {
             Wrapper wrapper = gson.fromJson(fr, Wrapper.class);
+            DatabaseHandler dbHandler = DatabaseHandler.getInstance();
             for (Review r: wrapper.reviewDetails.reviewCollection.review) {
                 if (r.getUserNickname().equals("")) {
                     r.setUserNickname("Anonymous");
                 }
-                this.reviews.computeIfAbsent(r.getHotelId(), k -> new TreeSet<>());
-                this.reviews.get(r.getHotelId()).add(r);
+                dbHandler.addReview(r);
             }
         } catch (IOException e) {
             System.out.println("Could not read the file: " + e);
@@ -54,12 +64,62 @@ public abstract class ReviewSearcher {
     }
 
     /**
+     * Traverse the given directory recursively and parse the review json files
+     * @param directory The directory to be traversed
+     */
+    protected void parseDirectoryMultiThread(String directory) {
+        Path p = Paths.get(directory);
+        try (DirectoryStream<Path> pathsInDir = Files.newDirectoryStream(p)) {
+            for (Path path : pathsInDir) {
+                if (!Files.isDirectory(path) && (path.toString().endsWith(".json"))){
+                    poolManager.execute(new ReviewParser(path.toString()));
+                    phaser.register();
+                }else {
+                    this.parseDirectoryMultiThread(path.toString());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Can not open directory: " + directory);
+        }
+    }
+
+    /**
+     * The inner class of ThreadSafeReviewSearcher that implements Runnable
+     */
+    public class ReviewParser implements Runnable {
+
+        private String file;
+        /**
+         * Constructor of ReviewParser
+         * @param file the json file to be parsed
+         */
+        public ReviewParser(String file) {
+            this.file = file;
+        }
+
+        /**
+         * The overridden run method that parse reviews
+         */
+        @Override
+        public void run() {
+            try {
+                parseReview(file);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                phaser.arriveAndDeregister();
+            }
+        }
+
+    }
+
+    /**
      * Return the set of reviews with a given hotel id
      * @param hotelId the hotel id
      * @return the set of reviews with the given hotel id
      */
-    public Set<Review> findReview(String hotelId) {
-        return this.reviews.get(hotelId);
+    public List<Review> findReview(String hotelId) {
+        return DatabaseHandler.getInstance().getReviewWithId(hotelId);
     }
 
     /**
@@ -69,15 +129,7 @@ public abstract class ReviewSearcher {
      * @return true if the user already has a review of the hotel
      */
     public boolean findSpecificReview (String hotelId, String username) {
-        if (this.reviews.get(hotelId) == null) {
-            return false;
-        }
-        for (Review review: this.reviews.get(hotelId)) {
-            if (review.getUserNickname() != null && review.getUserNickname().equals(username)) {
-                return true;
-            }
-        }
-        return false;
+        return DatabaseHandler.getInstance().getReviewWithName(hotelId, username);
     }
 
     /**
@@ -87,15 +139,9 @@ public abstract class ReviewSearcher {
      * @param title title of the review
      * @param text review content
      */
-    public void addReview(String username, String hotelId, String title, String text) {
-        Review review = new Review(hotelId, title, text, username, LocalDateTime.now().toString());
-        if (this.reviews.get(hotelId) != null) {
-            this.reviews.get(hotelId).add(review);
-        } else {
-            TreeSet<Review> reviewSet = new TreeSet<>();
-            reviewSet.add(review);
-            this.reviews.put(hotelId, reviewSet);
-        }
+    public void addReview(String username, String hotelId, String title, String text, int rating) {
+        Review review = new Review(hotelId, title, text, username, new java.sql.Date(System.currentTimeMillis()), rating);
+        DatabaseHandler.getInstance().addReview(review);
     }
 
     /**
@@ -104,12 +150,15 @@ public abstract class ReviewSearcher {
      * @param hotelId hotel id
      */
     public void deleteReview (String username, String hotelId) {
-        for (Review review: this.reviews.get(hotelId)) {
+        /*for (Review review: this.reviews.get(hotelId)) {
             if (review.getUserNickname().equals(username)) {
                 this.reviews.get(hotelId).remove(review);
                 break;
             }
-        }
+        }*/
 
+    }
+    public static void main (String[] args) {
+        ReviewSearcher reviewSearcher = new ReviewSearcher("input/reviews");
     }
 }
